192d2fef0e687f457a92335f9b69fc81fdcc67b5
Abed Murrar <abedmurrar15@gmail.com>
2018-05-04 04:24:22 +0300

    use CRLF instead of LF

--- movies-project/routes/index.js
+++ movies-project/routes/index.js
@@ -1,9 +1,9 @@
 var express = require('express')
 var router = express.Router()
 
-/* GET home page. */
+/* GET home page */
 router.get('/', function(req, res, next) {
-	res.render('index', { title: 'Express' })
+	res.send('<h1>Rarrum Cima</h1>')
 })
 
 module.exports = router
