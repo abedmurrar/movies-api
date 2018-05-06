bf48fba27c62450547ea5b825e59010f0b64f44f
Abed Murrar <abedmurrar15@gmail.com>
2018-05-07 02:59:40 +0300

    asd

--- movies-project/routes/movies.js
+++ movies-project/routes/movies.js
@@ -1,27 +1,25 @@
 var express = require('express')
 var MovieController = require('../controllers/MoviesController')
 var multer = require('multer')
-// var upload = multer({ dest: '../img/' })
+var path = require('path')
+var upload = multer({
+	fileFilter: function (req, file, cb) {
+
+		var filetypes = /jpeg|jpg|png|svg|gif|bmp/
+		var mimetype = filetypes.test(file.mimetype)
+		var extname = filetypes.test(path.extname(file.originalname).toLowerCase())
+
+		if (mimetype && extname) {
+			return cb(null, true)
+		}
+		cb('Error: File upload only supports the following filetypes - ' + filetypes)
+	}
+})
 var router = express.Router()
 
 router.get('/:id?', MovieController.get)
-// router.post('/', upload.single('poster'), MovieController.post)
+router.post('/', upload.single('poster'), MovieController.post)
 router.put('/:id', MovieController.put)
 router.delete('/:id', MovieController.delete)
 
 module.exports = router
-
-// var storage = multer.diskStorage({
-//     destination: function (req, file, cb) {
-//         cb(null, 'public/uploads/')
-//     },
-//     filename: function (req, file, cb) {
-//         crypto.pseudoRandomBytes(16, function (err, raw) {
-//             cb(null, raw.toString('hex') + Date.now() + '.' + mime.extension(file.mimetype));
-//         });
-//     }
-// });
-
-// var upload = multer({
-//     storage: storage
-// });
